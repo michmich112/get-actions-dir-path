37e804146174091033fd0a5055bb6493833711cc
Akumetsu <cantacuzene.michel@gmail.com>
2022-11-14 15:57:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 const collectStats = require('gh-action-stats');
 
 function main() {
+  console.log("gha-repo", process.env.GITHUB_ACTION_REPOSITORY);
   core.setOutput('dir-path', __dirname);
 }
 
